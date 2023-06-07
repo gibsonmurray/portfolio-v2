0a6b88e2f64ad75ed4c1f2e802cba8a0888ae406
gibsonmurray <gibmurrays@gmail.com>
2023-06-07 15:23:11 -0400

    removed screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,6 @@ module.exports = {
     content: ["./src/**/*.{html,js,jsx}"],
     theme: {
         extend: {},
-        screens: {
-            "2xs":"360px",
-            xs: "480px",
-            sm: "640px",
-            md: "768px",
-            lg: "1024px",
-            xl: "1280px",
-            "2xl": "1536px",
-            "3xl": "1920px",
-        },
     },
     plugins: [],
 };
